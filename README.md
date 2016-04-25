223626e97c78ab305ffd4834ef106122e449fa53
haydenelza <hayden.elza@gmail.com>
2016-04-24 21:48:10 -0500

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# Instagram-image-click
+# Instagram Image Click
 Open Instagram image in new tab.
+
+## Installation
+Install a userscript extension such as greasemonkey or tampermonkey:  
+Click here: http://tampermonkey.net
+
+
+Then, click this link to install the script:  
+https://github.com/HaydenElza/instagram-image-click/raw/master/instagram-image-click.user.js
+
+If you get a bunch of code instead of it installing, you can copy and paste the code as a new script in greasemonkey/tampermonkey.
+
+You can view the source code or contribute at:   
+https://github.com/HaydenElza/instagram-image-click/
+
+## Features
+1. Click on image in expanded view on profile or image page view to open image in new tab.
+2. Middle click to open image in new tab without focusing on it.
---
+++ instagram-image-click.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         Instagram Image Click
+// @namespace    http://elza.me
+// @version      0.1
+// @description  Click on images on instagram to open in new tab
+// @author       Hayden Elza
+// @match        https://www.instagram.com/*
+// @grant        http://code.jquery.com/jquery-latest.js
+// @updateURL    https://github.com/HaydenElza/instagram-image-click/raw/master/instagram-image-click.user.js
+// ==/UserScript==
+
+(function() {    
+    function addOnclick() {
+        var elements = $('._ovg3g');
+        elements.each(function() {
+            var reactid = $(this).attr('data-reactid');
+            if (reactid.indexOf("=1jpg") > -1 && $(this).hasClass("hasEvent") === false) {
+                var url = reactid.replace("=2",":").replace(/=1/g,".").replace(/.*\$/,"").replace(/\?.*/,"");
+                $(this).on("click",function(){window.open(url,'_blank');});
+                $(this).addClass("hasEvent");
+            }
+        });
+    }
+    $(document).ready(function() {
+        addOnclick();
+        $(document).on("hover",$('._ovg3g'),function(){
+            addOnclick();
+        });
+    });
+})();
